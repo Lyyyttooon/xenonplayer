898d303479c497d9336ebd82156fcde3236c121f
Lyyyttooon <84236702+Lyyyttooon@users.noreply.github.com>
2023-10-05 20:53:14 +0800

    feat: 进度条样式

--- src/components/VideoPlayer.vue
+++ src/components/VideoPlayer.vue
@@ -21,6 +21,7 @@ const handleFileChange = (e: Event) => {
   }
 }
 </script>
+
 <template>
   <div class="video-player">
     <video ref="videoElement" v-if="videoUrl !== ''" class="video" controls>
@@ -36,7 +37,16 @@ const handleFileChange = (e: Event) => {
         @change="handleFileChange"
       />
     </div>
-    <div class="control-bar"></div>
+    <div class="control-bar">
+      <div class="process-bar">
+        <div class="process-container">
+          <div class="scrubber-button"></div>
+          <div class="fill-line"></div>
+        </div>
+        <div class="volume-container"></div>
+      </div>
+      <div class="control-btns"></div>
+    </div>
   </div>
 </template>
 
@@ -65,5 +75,44 @@ const handleFileChange = (e: Event) => {
   background-color: #323233;
   height: 64px;
   flex: none;
+}
+
+.process-bar {
+  height: 24px;
+  border-bottom: 1px solid #000;
+  display: flex;
+
+  .process-container {
+    height: 100%;
+    width: calc(100% - 128px);
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    padding: 0 8px;
+
+    .fill-line {
+      height: 2px;
+      width: 100%;
+      background-color: #575656;
+    }
+
+    .scrubber-button {
+      height: 12px;
+      width: 12px;
+      border-radius: 6px;
+      background-color: #c9c9c9;
+      position: relative;
+      left: 0;
+      right: 0;
+      top: 0;
+      bottom: 0;
+      transform: translateX(0px);
+    }
+  }
+
+  .volume-container {
+    height: 100%;
+    width: 128px;
+  }
 }
 </style>
